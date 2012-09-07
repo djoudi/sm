b9bd03f71ec72785f4731fcc8fce5ac5596d1cd5
omid pourhadi <omidpourhadi@gmail.com>
2012-09-07 16:47:36 +0430

    add student list

--- src/main/com/sm/model/Student.java
+++ src/main/com/sm/model/Student.java
@@ -2,11 +2,18 @@ package com.sm.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "student", uniqueConstraints = { @UniqueConstraint(columnNames = "addmission_no") })
 public class Student extends StudentVO
 {
+
+	@Transient
+	public String getFullName()
+	{
+		return getFirstName() + " " + getLastName();
+	}
 
 }
